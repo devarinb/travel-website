a028cf594156973261b5783d59cc3247f5e00732
Arindam Biswas <mail@arindambiswas.co.in>
2024-01-29 02:21:26 +0530

    fix-config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Remove the line below if you want to use Image Optimization in server mode
   output: 'export',
-  images: {
-    domains: [], // Add the domains from where you want to load images
-    path: '/_next/image',
-    loader: 'default',
-    formats: ['image/avif'],
-  },
+
+  // Add the line below if you want to disable Image Optimization
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
