6d761e9f1c973faff9279bf43ee1447860b18be2
Arindam Biswas <mail@arindambiswas.co.in>
2024-01-29 01:47:16 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Remove the line below if you want to use Image Optimization in server mode
+  //output: 'export',
+
+  // Add the line below if you want to disable Image Optimization
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
