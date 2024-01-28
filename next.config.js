726af6fa6a6b44c59d5e501bdc1ca459ecc15bfe
Arindam Biswas <mail@arindambiswas.co.in>
2024-01-29 01:57:22 +0530

    fix-config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove the line below if you want to use Image Optimization in server mode
-  //output: 'export',
+  output: 'export',
 
   // Add the line below if you want to disable Image Optimization
   images: { unoptimized: true },
